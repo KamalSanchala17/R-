67596e7ba1990c48de55acb98facd9139b3569d2
KAMAL SANCHALA <112723265+KamalSanchala17@users.noreply.github.com>
2022-09-06 11:12:30 +0530

    Add files via upload

---
+++ R Assignment Functions answer.R
@@ -0,0 +1,73 @@
+
+
+#1. Create function for sum of two objects. 
+
+sm = function(a=0,b=0){
+  print(a+b)
+}
+sm(5,3)  
+
+#2. Create function for Subtract of two objects. 
+minus  = function(a=0,b=0){
+  print(a-b)
+}
+
+minus(20,25)
+
+
+#3. Create function for multiply of two objects. 
+
+
+multi = function(a=0,b=0){
+  print(a*b)
+}
+
+multi(2,3)
+
+
+# 4. Create a function for (A^3)+(B^2)-(C^2). 
+
+stat = function(A=0,B=0,C=0){
+  print((A^3)+(B^2)-(C^2))
+}
+
+stat(5,5,5)
+
+#5. Create a function for print First name and Last name of user with Hello massage.
+ 
+new_k = function(a = 'hello',b){
+  print(b,a)
+}
+
+new_k('kamal sanchala',)
+
+
+
+#6. Create a function to get the range of the vector. Hint (max-min). 
+myfun = function(a){
+  for(i in 1:a){
+    print (i)
+  }
+}
+myfun(10)
+
+
+#7. Create a function for getting square root of any number. Hint ( sqrt(x) ).
+
+rute = function(x){
+  print(sqrt(x))
+}
+rute(10)
+
+
+#8. Create a function for identify the number of characters in string. 
+
+c= function(a){
+  print(nchar(a))
+}
+
+c('kamal')
+
+
+
+
